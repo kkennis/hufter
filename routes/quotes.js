b8ea933878f1d536b7758bff3ca24be9c7f39c93
kkennis <kevinvkennis@gmail.com>
2015-11-16 19:46:50 -0800

    Add specific tickers with route

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var YFquotes = require('../queries/quotes.js');
-var saveToMongo = require('../queries/save.js')
+var saveToMongo = require('../queries/save.js');
+var mongoose = require('mongoose');
+var uniq = require('uniq')
+
+var tickerSchema, Ticker, db;
+
+tickerSchema = mongoose.Schema({ name: String });
+
+Ticker = mongoose.model('Ticker', tickerSchema);
+
+db = mongoose.connection;
+
 
 router.get('/', function(req, res, next) {
   var response;
@@ -20,16 +31,54 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/save', function(req, res, next) {
-  var symbols = ["SCON", "YELP", "P", "TWTR", "DIG", "BBRY", "WDC", "OIH", "IVE", "IBM"];
-  response = YFquotes.getLastTrade(symbols);
-  if (Object.keys(response).length !== 0){
-    saveToMongo(response);
-    res.end("Request received at " + new Date().toString());
-  } else {
-    res.send(response);
-    res.end("Yahoo API Error")
-  }
+  console.log("Saving...");
+
+  mongoose.connect('mongodb://localhost/test');
+
+
+  db.once('open', function(){
+      Ticker.find({},function(err, tickers){
+        var symbols = tickers.map(function(ticker){
+          return ticker["name"];
+        })
+
+        new Promise(function(resolve, reject){
+          response = YFquotes.getLastTrade(uniq(symbols));
+          resolve(response);
+        })
+        .then(function(response){
+          if (Object.keys(response).length !== 0){
+            saveToMongo(response);
+            res.end("Request received at " + new Date().toString());
+          } else {
+            res.send(response);
+            res.end("Yahoo API Error")
+          }
+        })
+      });
+  });
+
 });
 
+router.get('/save/:ticker', function(req, res, next){
+  console.log("Saving ticker...", req.params.ticker.toString('utf8'));
+  mongoose.connect('mongodb://localhost/test');
+
+  db.once('open', function(){
+    var newSymbol = new Ticker({ name: req.params.ticker.toString('utf8') });
+    
+    newSymbol.save(function(err, symbol){
+      if (err){
+        res.end("Error saving stock")
+        mongoose.disconnect();
+      } else {
+        res.end("Successfully saved stock")
+        mongoose.disconnect();
+      }
+    });
+
+  });
+
+});
 
 module.exports = router;
