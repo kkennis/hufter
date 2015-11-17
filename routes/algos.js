4a7373f20aa79df919c842ef6e78b596198b7153
kkennis <kevinvkennis@gmail.com>
2015-11-16 20:17:12 -0800

    Add algo decryption

--- routes/algos.js
+++ routes/algos.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var YFhistoricaldata = require('../queries/historicaldata.js');
-var backtest = require('../backtester/backtester.js')
-var algo = require('../testalgo.js')
+var backtest = require('../backtester/backtester.js');
+var algo = require('../testalgo.js');
+var CryptoJS = require("crypto-js");
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
+
+var key = "yolocity";
+
+function aesDecrypt(string,key){
+  var decrypted = AES.decrypt(string, key);
+  var decryptedString = CryptoJS.enc.Utf8.stringify(decrypted);
+  return decryptedString;
+}
+
+function aesEncrypt(string,key){
+  var encrypted = AES.encrypt(string,key);
+  var encryptedString = encrypted.toString();
+  return encryptedString;
+}
 
 router.post('/', function(req, res, next){
   // var encryptedAlgo = req.body.data;
@@ -12,14 +29,18 @@ router.post('/', function(req, res, next){
   // How to best send post data?
 
   new Promise(function(resolve, reject){
-    var stockData = YFhistoricaldata.getAllData(JSON.parse(req.body.symbols), req.body.startDate, req.body.endDate)
+    var data = JSON.parse(aesDecrypt(req.body.data, key));
+
+    var testingAlgo = (new Function('return ' + data.algo))(); 
+
+    var stockData = YFhistoricaldata.getAllData(JSON.parse(data.symbols), data.startDate, data.endDate);
 
     if (stockData) { resolve(stockData); } 
     else { reject(Error("API Error")); }
   })
   .then(function(data){
     // backtest(decryptedAlgo, JSON.parse(req.body.symbols, data.results) );
-    return backtest(algo, JSON.parse(req.body.symbols), data.results);
+    return backtest(data.algo, JSON.parse(req.body.symbols), data.results);
   })
   .then(function(results){ res.json(results); },
         function(error){ res.send(error); });  
