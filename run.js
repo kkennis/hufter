a07554d7e825a100f6c642c8a0d92e409274f065
kkennis <kevinvkennis@gmail.com>
2015-11-16 18:55:35 -0800

    Only send request when trading hours

--- run.js
+++ run.js
@@ -1,15 +1,22 @@
 var xhr = require('xmlhttprequest');
+var mongoose = require('mongoose');
+var moment = require('moment');
+var tz = require('moment-timezone');
+var range = require('moment-range');
+
 
 setInterval(function(){ 
 
+  var currentTime = moment().tz('America/New_York');
+  var openTime = moment().tz('America/New_York').hours(9).minutes(30).seconds(0);
+  var closeTime = moment().tz('America/New_York').hours(16).minutes(0).seconds(0);
+  var tradingHours = moment.range(openTime, closeTime)
 
-
-  console.log("Sending request at", new Date())
-  var XMLHttpRequest = xhr.XMLHttpRequest;
-  var request = new XMLHttpRequest();
-  request.open('GET', "http://localhost:3000/quotes/save", true);
-
-  request.onload = function()
-
-  request.send();
+  if (currentTime.within(tradingHours)){
+    console.log("Sending request at", new Date())
+    var XMLHttpRequest = xhr.XMLHttpRequest;
+    var request = new XMLHttpRequest();
+    request.open('GET', "http://localhost:3000/quotes/save", true);
+    request.send();
+  }
 }, 30000);
