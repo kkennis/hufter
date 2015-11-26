08f70655847f5f66644cb2defba0a0eb4f16ab9f
kkennis <kevinvkennis@gmail.com>
2015-11-25 22:38:24 -0800

    Remove comments

--- db/query-mongo.js
+++ db/query-mongo.js
@@ -12,41 +12,12 @@ var host = process.env["DB_HOST"]
 var tickerSchema = mongoose.Schema({ name: String });
 var Ticker = mongoose.model('Ticker', tickerSchema);
 
-// Functional mongo calls
-
 function getTickers(){
   return Ticker
     .find({})
     .exec()
     .then((tickers) => _.pluck('name', tickers))
     .then(uniq);
-    // .then((symbols) => YFquotes.getLastTrade(symbols))
-    // .then((response) => saveToMongo(response));
 }
-
-// function saveToMongo(data){
-
-// }
-
-
-// var getTickers = Ticker.find({},function(err, tickers){
-//   var symbols = tickers.map(function(ticker){
-//     return ticker["name"];
-//   })
-
-//   new Promise(function(resolve, reject){
-//     response = YFquotes.getLastTrade(uniq(symbols));
-//     resolve(response);
-//   })
-//   .then(function(response){
-//     if (Object.keys(response).length !== 0){
-//       saveToMongo(response);
-//       res.end("Data saved at " + new Date().toString());
-//     } else {
-//       res.send(response);
-//       res.end("Yahoo API Error")
-//     }
-//   })
-// });
 
 module.exports = getTickers;
