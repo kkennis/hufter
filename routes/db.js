70d381fac55a07057705560559ec88616e11ce2e
kkennis <kevinvkennis@gmail.com>
2015-11-25 22:37:29 -0800

    Remove comments

--- routes/db.js
+++ routes/db.js
@@ -11,7 +11,6 @@ var host = process.env["DB_HOST"]
 
 var getTickers = require('../db/query-mongo.js');
 var saveToMongo = require('../db/save-mongo.js');
-// var tickerSchema = mongoose.Schema({ name: String });
 var Ticker = mongoose.model('Ticker');
 var tickers;
 
@@ -22,45 +21,16 @@ router.use('*', function(req, res, next){
   } else {
     next();
   }
-   
+
 })
 
 router.get('/save', function(req, res, next) {
   console.log("Saving...");
 
-  // Make this async sync
   getTickers()
     .then((tickers) => YFquotes.getLastTrade(tickers))
     .then((response) => saveToMongo(response))
-    .then((saveStatuses, err) => res.end(err || saveStatuses)) 
-
-  // var tickers = getTickers();
-  // var response = YFquotes.getLastTrade(tickers);
-  // var saveStatus = saveToMongo(response)
-  // res.end(saveStatus);
-
-
-
-
-  // Ticker.find({},function(err, tickers){
-  //   var symbols = tickers.map(function(ticker){
-  //     return ticker["name"];
-  //   })
-
-  //   new Promise(function(resolve, reject){
-  //     response = YFquotes.getLastTrade(uniq(symbols));
-  //     resolve(response);
-  //   })
-  //   .then(function(response){
-  //     if (Object.keys(response).length !== 0){
-  //       saveToMongo(response);
-  //       res.end("Data saved at " + new Date().toString());
-  //     } else {
-  //       res.send(response);
-  //       res.end("Yahoo API Error")
-  //     }
-  //   })
-  // });
+    .then((saveStatuses, err) => res.end(err || saveStatuses))
 });
 
 router.get('/save/all', function(req, res, next){
@@ -70,7 +40,7 @@ router.get('/save/all', function(req, res, next){
 
 router.get('/save/:ticker', function(req, res, next){
   console.log("Saving ticker...", req.params.ticker.toString('utf8'));
-  var newSymbol = new Ticker({ name: req.params.ticker.toString('utf8') });    
+  var newSymbol = new Ticker({ name: req.params.ticker.toString('utf8') });
 
   newSymbol.save()
     .then((data, err) => res.end(err || "Successfully saved stock"))
