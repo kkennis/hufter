ac0ab4415065c80847b6efbd91f56f2461719d57
kkennis <kevinvkennis@gmail.com>
2015-12-21 18:49:05 -0800

    Get to point of executing algo

--- native/test.cpp
+++ native/test.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 #include "json/json-forwards.h"
 #include "json/json.h"
 #include "jsoncpp.cpp"
-#include <fstream>
 
 // #include <algo.h>
 using namespace std;
@@ -18,8 +20,8 @@ using namespace std;
 // Turn array output into stringified JSON
 // Cout JSON string
 
-struct quote {
-  int timestamp;
+struct Quote {
+  string timestamp;
   double price;
 };
 
@@ -27,18 +29,49 @@ int main(int argc, char* argv[]) {
   string data = "";
   cin >> data;
 
-  Json::Value array;
-  array.append("hello");
-  array.append("world");
-
   Json::Value parsedData;
   Json::Reader reader;
   Json::StyledWriter writer;
   bool parsingSuccessful = reader.parse(data, parsedData);
   if (parsingSuccessful)
   {
-    string output = writer.write(parsedData);
+    ofstream fout("results.txt");
+    // Go to Results
+    // For each key of results (i.e stock)
+    // Create vector of quotes (our struct)
+    // For each day in stock, create struct with close and data
+
+    Json::Value output;
+    Json::Value results = parsedData["results"];
+    vector<string> symbols = results.getMemberNames();
+
+    for (auto symbol: symbols) {
+
+      Json::Value quoteData = results[symbol];
+      vector<Quote> stockData;
+      fout << symbol << endl;
+      fout << "=======================================" << endl;
+      for (Json::Value::iterator it = quoteData.begin(); it != quoteData.end(); ++it) {
+        Quote dayData;
+        dayData.price = stod((*it)["Close"].asString());
+        dayData.timestamp = (*it)["Date"].asString();
+        stockData.push_back(dayData);
+      }
+      vector<quote> signals[2] = algo(stockData);
+
+      // Now we need to reconstruct output
+      // output[symbol]["buy"] = signals[0];
+      // output[symbol]["sell"] = signals[1];
+
+      fout << "***************************************" << endl;
+    }
+
+    fout << endl;
+
+    string output = writer.write(results);
     cout << output << endl;
+
+    fout.close();
   }
 
 
